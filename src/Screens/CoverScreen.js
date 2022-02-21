3dcca73d0f770fdda0ab67db64e2a129deef5551
Madhu683 <madhu.kandukuri419@gmail.com>
2022-02-21 22:24:30 +0530

    CoverScreenstyles

--- src/Screens/CoverScreen.js
+++ src/Screens/CoverScreen.js
@@ -202,4 +202,99 @@ function CoverScreen() {
   )
 }
 
+const style = StyleSheet.create({
+    Container:{
+        flex:1,
+        backgroundColor:'#FFFFFF',
+    },
+    BackgoundImage:{
+        flex:1,
+        position:'absolute',
+        width:Dimensions.get('screen').width,
+        height:Dimensions.get('screen').height,
+        left:0,
+        top:0,
+    },
+    BackgoundLinearGradient:{
+        flex:1,          
+        width:Dimensions.get('screen').width,
+        height:Dimensions.get('screen').height,
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    LogoView:{
+    position:'absolute',
+    flexDirection:'row',
+    top:40,
+   },
+   LogoImage:{
+      margin:5,
+   },
+   LogoTitle:{
+    margin:5,
+    fontFamily:'Poppins_500Medium',
+    fontSize:15,
+    letterSpacing:0.2,
+   },
+   SubContainer:{
+    flex:1,             
+    position:'absolute',
+    left:24,
+    right:24,
+    top:517,
+    width:Dimensions.get('screen').width-48,
+    height:144,
+    justifyContent:'center',
+    alignItems:'center',
+   },
+   Headline:{
+    fontFamily:'Poppins_600SemiBold',
+    fontSize:32,
+    fontWeight:'600',
+    fontStyle:'normal',
+    lineHeight:42,
+    color:'#FFFFFF',
+   },
+   Subline1:{
+    marginTop:27,
+    fontSize:13,
+    fontFamily:'Poppins_400Regular',
+    fontWeight:'normal',
+    fontStyle:'normal',
+    lineHeight:22,
+    color:'#FFFFFF',
+    opacity:0.6,   
+   },
+   Subline2:{
+    fontSize:13,
+    fontFamily:'Poppins_400Regular',
+    fontWeight:'normal',
+    fontStyle:'normal',
+    lineHeight:22,
+    color:'#FFFFFF',
+    opacity:0.6,   
+   },
+   Button:{
+    height:56,
+    width:149,
+    backgroundColor:'#52B6DF',
+    borderRadius:10,
+    alignItems:'center',
+    justifyContent:'center',
+    top:40,
+   },
+   ButtonText:{
+    fontFamily:'Poppins_600SemiBold',   
+    color:"#FFFFFF",
+    fontSize:17,
+    fontWeight:'600',
+    lineHeight:24, 
+   }
+   
+
+
+})
+
+
+
 export default CoverScreen
